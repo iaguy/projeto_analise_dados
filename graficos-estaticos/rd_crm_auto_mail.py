6da089928692a4982fe2b8fd7fd746f4f514b8db
iaguy <brunoiaguy@hotmail.com>
2023-10-28 09:17:15 -0300

    fix: grafico dinamico auto mail

--- graficos-estaticos/rd_crm_auto_mail.py
+++ graficos-estaticos/rd_crm_auto_mail.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import plotly.express as px
+import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 
 # Conexão com a base de dados
@@ -13,8 +13,8 @@ df = pd.read_sql(query, engine)
 df = df.drop(['created_at', 'updated_at', 'id'], axis=1)
 df.dropna(inplace=True)
 
-# Renomear as colunas para uso no Plotly
-df = df.rename(columns={
+
+ax = df.rename(columns={
     'email_name': 'Nome do Email',
     'contacts_count': 'Contagem de Contatos',
     'email_delivered_count': 'Contagem de Emails Entregues',
@@ -23,20 +23,20 @@ df = df.rename(columns={
     'email_dropped_count': 'Contagem de Emails Descartados',
     'email_unsubscribed_count': 'Contagem de Cancelamentos de Inscrição em Emails',
     'email_spam_reported_count': 'Contagem de Emails Marcados como Spam'
-})
+}).plot.bar(x='Nome do Email',
+            y=[
+                'Contagem de Contatos',
+                'Contagem de Emails Entregues',
+                'Contagem de Emails Abertos Únicos',
+                'Contagem de Cliques em Emails Únicos',
+                'Contagem de Emails Descartados',
+                'Contagem de Cancelamentos de Inscrição em Emails',
+                'Contagem de Emails Marcados como Spam'
+            ],
+            rot=0, figsize=(14, 8))
 
-# Crie um gráfico de barras Plotly
-fig = px.bar(df,
-             x='Nome do Email',
-             y=['Contagem de Contatos', 'Contagem de Emails Entregues', 'Contagem de Emails Abertos Únicos',
-                'Contagem de Cliques em Emails Únicos', 'Contagem de Emails Descartados',
-                'Contagem de Cancelamentos de Inscrição em Emails', 'Contagem de Emails Marcados como Spam'],
-             title="Estatísticas de Email Marketing",
-             labels={'value': 'Contagem'},
-             height=700,
-             width=1200)
+plt.xticks(rotation=0)
 
-# Salva o gráfico em arquivo HTML
-fig.write_html('../graficos/dinamicos/rd_crm_auto_mail.html')
-# Mostra o gráfico Plotly
-fig.show()
+# Mostra o Grafico
+plt.savefig('../graficos/rd_mkt_auto_mail.png')
+plt.show()
